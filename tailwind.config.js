660b3a1e462864ba658e5ad80380fda0a5b93ef8
chankuzy <mchankuxieey@gmail.com>
2025-07-22 21:28:07 -0700

    Tailwind downgraded to v3 and everything works across devices

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{html,js,ts,jsx,tsx}',
+    './src/**/*.{vue,html,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
